85a532707858d356aedecd74454db241da555d05
Edson Mallet <edsonmmr@gmail.com>
2021-01-26 10:17:35 -0300

    feat: add repositories, services, entities from heroes

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const http = require('http')
+const PORT = 3000
+const DEFAULT_HEADER = { 'Content-Type': 'application/json' }
+const HeroFactory = require('./factories/heroFactory')
+const heroService = HeroFactory.generateInstance()
+
+const routes = {
+    '/heroes:get': async(request, response) => {
+        const { id } = request.queryString
+        const heroes = await heroService.find(id)
+        response.write(JSON.stringify({ results: heroes }))
+        return response.end()
+    },
+    default: (request, response) => {
+        response.write('Hello!')
+        response.end()
+    }
+}
+
+const handleError = response => {
+    return error => {
+        console.error('Deu Ruim!***', error)
+        response.writeHead(500, DEFAULT_HEADER)
+        response.write(JSON.stringify({ error: 'Internal Server Error!!' }))
+
+        return response.end()
+    }
+}
+
+const handler = (request, response) => {
+    const { url, method } = request
+    const [first, route, id] = url.split('/')
+    request.queryString = { id: isNaN(id) ? id : Number(id) }
+
+    const key = `/${route}:${method.toLowerCase()}`
+
+    response.writeHead(200, DEFAULT_HEADER)
+
+    const chosen = routes[key] || routes.default
+    return chosen(request, response).catch(handleError(response))
+
+}
+
+http.createServer(handler).listen(PORT, () => console.log('server running at port: ', PORT))
